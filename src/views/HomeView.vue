2a5b0cbfee23da0540e6817d035af530b430b183
sharyar-dev <onshariiit@gmail.com>
2025-04-15 15:50:02 +0500

    feat: add Tailwind CSS configuration with shadcn library

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
 <template>
   <div>
     <h1>This is home page</h1>
+    <Button>Click me</Button>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,10 +12,11 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
